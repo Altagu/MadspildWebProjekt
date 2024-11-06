18429fe30e90a466d046ca89e0af85bde9609b4d
Altagu <alexgunnersen25@gmail.com>
2024-11-06 12:03:42 +0100

    First draft

---
+++ src/main/java/com/example/madspildwebprojekt/Controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.madspildwebprojekt.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "home/index";
+    }
+
+    @GetMapping("/forbruger")
+    public String forbruger() {
+        return "home/forbruger";
+    }
+
+    @GetMapping("/organisation")
+    public String organisation() {
+        return "home/organisation";
+    }
+
+    @GetMapping("/virksomhed")
+    public String virksomhed() {
+        return "home/virksomhed";
+    }
+}
